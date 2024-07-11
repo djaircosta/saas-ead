98dc677f9f5720a4f86667aab1a70d99f3ad6167
DjairCosta <riajd10@gmail.com>
2024-07-11 06:16:27 -0300

    feat: Creat project mobile

---
+++ Ead.Api/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Ead.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ead.Api;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    }
+}
